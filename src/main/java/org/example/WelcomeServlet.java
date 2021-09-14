755046514f4f262d5ed1db8297c24ffbec06f729
EgorYakovlevAcc <yakovlev.es@phystech.edu>
2021-09-14 13:39:50 +0300

    logger lection

--- src/main/java/org/example/WelcomeServlet.java
+++ src/main/java/org/example/WelcomeServlet.java
@@ -6,13 +6,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WelcomeServlet extends HttpServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WelcomeServlet.class);
 
     @Override
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
             throws IOException, ServletException {
+
+        LOGGER.log(Level.INFO, "Welcome page has been successfully uploaded.");
 
         RequestDispatcher view = request.getRequestDispatcher("welcome.jsp");
         view.forward(request, response);
